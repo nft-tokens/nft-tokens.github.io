c4aff2743ef665822e3cb627bad0938c5347b707
Redberry <redberry.dev@protonmail.com>
2021-09-25 21:52:39 +0200

    a

--- scripts/preview.js
+++ scripts/preview.js
@@ -7,7 +7,7 @@ import "./html2canvas.js";
 html2canvas(document.querySelector("#canvas")).then(canvas => {
     //document.body.appendChild(canvas)
     let link = document.createElement('a');
-    link.download = `bird_${Date.now()}.png`;
+    link.download = `pixel_${Date.now()}.png`;
     link.href = canvas.toDataURL()
     //link.click();
 });
